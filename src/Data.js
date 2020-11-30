1a9ec5aa1234990c16031dd9544f6d93d9b38539
michaeldai1006 <dtc74110@gmail.com>
2020-11-29 23:09:26 -0700

    Remove storage package

--- src/Data.js
+++ src/Data.js
@@ -1,18 +1,11 @@
-import { Storage } from '@stacks/storage';
 
 export const save = async (userSession, info) => {
-    // New storage instance
-    const storage = new Storage({ userSession });
-
     // Put file
     await userSession.putFile("D-Vault-Info", info, 
     { encrypt: false, dangerouslyIgnoreEtag: true});
 };
 
 export const fetch = async (userSession, username) => {
-    // New storage instance
-    const storage = new Storage({ userSession });
-
     // Get file
     const info = await userSession.getFile("D-Vault-Info", {
         decrypt: false,
